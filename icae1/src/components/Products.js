a7ca16fc82f66887231eeeec290694c210756fca
Madhuranga Bandara <madurangabandara2346@gmail.com>
2024-12-03 15:44:02 +0000

    products file bug fixed

--- icae1/src/components/Products.js
+++ icae1/src/components/Products.js
@@ -1,16 +1,14 @@
-
 import '../assets/CSS/layout.css';
 import Product from './Product';
 import Cart from './Cart';
-import { flowers } from './FlowerDb';
-import {useState} from 'react'
 import { flowers } from './FlowerDB';
+import {useState} from 'react'
 
 
 export default function Products(){
-    const [cart,setCart]=useState([]);
+     const [cart,setCart]=useState([]);
 
-    const AddToCart =(flower) =>{
+     const AddToCart =(flower) =>{
         const existingItem = cart.find((item) => item.id === flower.id)
         if(existingItem){
             setCart(
@@ -32,21 +30,21 @@ export default function Products(){
                 <h4 className="card-title">Buy flowers</h4>
                 <div className="grid-container">
                     {
-                        //product
                         flowers.map((flower)=>(
                             <Product 
                                key={flower.id}
                                flower={flower}
+                               onAddToCart={AddToCart}
                                />
+
                         )
+                    )
                     }
                 </div>
 
             </div>
             <div className="item3">
-                {
-                //cart
-                }
+                <Cart cart={cart}/>
             </div>
         </>
     );
